e0b7352e06e69b0edb85fa4202e33142efa6ebd9
raffipython <raffi@jubrael.com>
2022-12-22 14:38:42 -0500

    Draft v0.1
    None

---
+++ lab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Initial setup
+sudo apt-get -y install terminator docker docker-compose
+sudo docker pull debian:buster
+sudo docker images |grep buster
+sudo docker build -t labimage .
+sudo docker images |grep labimage
+
+# Create containers
+sudo docker container list | grep lab_ && sudo docker-compose down --remove-orphans
+sudo docker-compose up -d
+sudo docker container list
+
+# Drop to shell on box 1
+sudo docker exec --privileged -it lab_container1_1 /bin/bash
+
